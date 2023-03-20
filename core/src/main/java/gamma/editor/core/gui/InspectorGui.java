baf6d0a7108b916beed7366d09c349c7f7013aad
Nico <nico.hex6@gmail.com>
2023-03-20 19:40:29 +0100

    Range and slider annotations

--- core/src/main/java/gamma/editor/core/gui/InspectorGui.java
+++ core/src/main/java/gamma/editor/core/gui/InspectorGui.java
@@ -1,7 +1,6 @@
 package gamma.editor.core.gui;
 
-import gamma.engine.core.annotations.EditorRange;
-import gamma.engine.core.annotations.EditorVariable;
+import gamma.engine.core.annotations.*;
 import gamma.engine.core.resources.Resource;
 import gamma.engine.core.resources.Resources;
 import gamma.engine.core.scene.Component;
@@ -9,15 +8,18 @@ import gamma.engine.core.scene.Entity;
 import gamma.engine.core.window.Window;
 import imgui.ImGui;
 import imgui.flag.ImGuiCond;
+import imgui.type.ImFloat;
+import imgui.type.ImInt;
 import imgui.type.ImString;
 import vecmatlib.vector.Vec2f;
 import vecmatlib.vector.Vec2i;
 import vecmatlib.vector.Vec3f;
 import vecmatlib.vector.Vec4f;
 
+import java.io.UncheckedIOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.NoSuchElementException;
 
@@ -84,24 +86,55 @@ public class InspectorGui implements IEditorGui {
 	}
 
 	private static void renderRealField(Component component, Field field) throws IllegalAccessException {
-		EditorRange range = field.getAnnotation(EditorRange.class);
-		float[] ptr = {(float) field.getDouble(component)};
-		float step = range != null ? range.step() : 0.001f;
-		float min = range != null ? range.min() : Float.NEGATIVE_INFINITY;
-		float max = range != null ? range.max() : Float.POSITIVE_INFINITY;
-		if(ImGui.dragFloat("##" + field.getName(), ptr, step, min, max)) {
-			field.set(component, ptr[0]);
+		if(field.isAnnotationPresent(EditorRange.class)) {
+			EditorRange range = field.getAnnotation(EditorRange.class);
+			float value = (float) field.getDouble(component);
+			float[] ptr = {field.isAnnotationPresent(EditorDegrees.class) ? (float) Math.toDegrees(value) : value};
+			float step = range != null ? range.step() : 0.001f;
+			float min = range != null ? range.min() : Float.NEGATIVE_INFINITY;
+			float max = range != null ? range.max() : Float.POSITIVE_INFINITY;
+			if(ImGui.dragFloat("##" + field.getName(), ptr, step, min, max)) {
+				field.set(component, field.isAnnotationPresent(EditorDegrees.class) ? (float) Math.toRadians(ptr[0]) : ptr[0]);
+			}
+		} else if(field.isAnnotationPresent(EditorSlider.class)) {
+			EditorSlider slider = field.getAnnotation(EditorSlider.class);
+			float[] ptr = {(float) field.getDouble(component)};
+			float min = slider != null ? slider.min() : Float.NEGATIVE_INFINITY;
+			float max = slider != null ? slider.max() : Float.POSITIVE_INFINITY;
+			if(ImGui.sliderFloat("##" + field.getName(), ptr, min, max)) {
+				field.set(component, ptr[0]);
+			}
+		} else {
+			ImFloat ptr = new ImFloat((float) field.getDouble(component));
+			if(ImGui.inputFloat("##" + field.getName(), ptr)) {
+				field.set(component, ptr.get());
+			}
 		}
 	}
 
 	private static void renderIntegerField(Component component, Field field, float defaultMin, float defaultMax) throws IllegalAccessException {
-		EditorRange range = field.getAnnotation(EditorRange.class);
-		int[] ptr = {(int) field.getLong(component)};
-		float step = range != null ? range.step() : 1.0f;
-		float min = range != null ? range.min() : defaultMin;
-		float max = range != null ? range.max() : defaultMax;
-		if(ImGui.dragInt("##" + field.getName(), ptr, step, min, max)) {
-			field.set(component, ptr[0]);
+		if(field.isAnnotationPresent(EditorRange.class)) {
+			EditorRange range = field.getAnnotation(EditorRange.class);
+			int[] ptr = {(int) field.getLong(component)};
+			float step = range != null ? range.step() : 0.001f;
+			float min = range != null ? range.min() : defaultMin;
+			float max = range != null ? range.max() : defaultMax;
+			if(ImGui.dragInt("##" + field.getName(), ptr, step, min, max)) {
+				field.set(component, ptr[0]);
+			}
+		} else if(field.isAnnotationPresent(EditorSlider.class)) {
+			EditorSlider slider = field.getAnnotation(EditorSlider.class);
+			int[] ptr = {(int) field.getLong(component)};
+			int min = (int) (slider != null ? slider.min() : defaultMin);
+			int max = (int) (slider != null ? slider.max() : defaultMax);
+			if(ImGui.sliderInt("##" + field.getName(), ptr, min, max)) {
+				field.set(component, ptr[0]);
+			}
+		} else {
+			ImInt ptr = new ImInt((int) field.getLong(component));
+			if(ImGui.inputInt("##" + field.getName(), ptr)) {
+				field.set(component, ptr.get());
+			}
 		}
 	}
 
@@ -120,46 +153,76 @@ public class InspectorGui implements IEditorGui {
 	}
 
 	private static void renderVecField2(Component component, Field field) throws IllegalAccessException {
-		EditorRange range = field.getAnnotation(EditorRange.class);
 		Vec2f vector = (Vec2f) field.get(component);
 		float[] ptr = {vector.x(), vector.y()};
-		float step = range != null ? range.step() : 0.001f;
-		float min = range != null ? range.min() : Float.NEGATIVE_INFINITY;
-		float max = range != null ? range.max() : Float.POSITIVE_INFINITY;
-		if(ImGui.dragFloat2("##" + field.getName(), ptr, step, min, max)) {
+		if(field.isAnnotationPresent(EditorRange.class)) {
+			EditorRange range = field.getAnnotation(EditorRange.class);
+			float step = range != null ? range.step() : 0.001f;
+			float min = range != null ? range.min() : Float.NEGATIVE_INFINITY;
+			float max = range != null ? range.max() : Float.POSITIVE_INFINITY;
+			if(ImGui.dragFloat2("##" + field.getName(), ptr, step, min, max)) {
+				field.set(component, new Vec2f(ptr[0], ptr[1]));
+			}
+		} else if(ImGui.inputFloat2("##" + field.getName(), ptr)) {
 			field.set(component, new Vec2f(ptr[0], ptr[1]));
 		}
 	}
 
 	private static void renderVecField3(Component component, Field field) throws IllegalAccessException {
-		EditorRange range = field.getAnnotation(EditorRange.class);
 		Vec3f vector = (Vec3f) field.get(component);
 		float[] ptr = {vector.x(), vector.y(), vector.z()};
-		float step = range != null ? range.step() : 0.001f;
-		float min = range != null ? range.min() : Float.NEGATIVE_INFINITY;
-		float max = range != null ? range.max() : Float.POSITIVE_INFINITY;
-		if(ImGui.dragFloat3("##" + field.getName(), ptr, step, min, max)) {
+		if(field.isAnnotationPresent(EditorRange.class)) {
+			EditorRange range = field.getAnnotation(EditorRange.class);
+			float step = range != null ? range.step() : 0.001f;
+			float min = range != null ? range.min() : Float.NEGATIVE_INFINITY;
+			float max = range != null ? range.max() : Float.POSITIVE_INFINITY;
+			if(ImGui.dragFloat3("##" + field.getName(), ptr, step, min, max)) {
+				field.set(component, new Vec3f(ptr[0], ptr[1], ptr[2]));
+			}
+		} else if(ImGui.inputFloat3("##" + field.getName(), ptr)) {
 			field.set(component, new Vec3f(ptr[0], ptr[1], ptr[2]));
 		}
 	}
 
 	private static void renderVecField4(Component component, Field field) throws IllegalAccessException {
-		EditorRange range = field.getAnnotation(EditorRange.class);
 		Vec4f vector = (Vec4f) field.get(component);
 		float[] ptr = {vector.x(), vector.y(), vector.z(), vector.w()};
-		float step = range != null ? range.step() : 0.001f;
-		float min = range != null ? range.min() : Float.NEGATIVE_INFINITY;
-		float max = range != null ? range.max() : Float.POSITIVE_INFINITY;
-		if(ImGui.dragFloat4("##" + field.getName(), ptr, step, min, max)) {
+		if(field.isAnnotationPresent(EditorRange.class)) {
+			EditorRange range = field.getAnnotation(EditorRange.class);
+			float step = range != null ? range.step() : 0.001f;
+			float min = range != null ? range.min() : Float.NEGATIVE_INFINITY;
+			float max = range != null ? range.max() : Float.POSITIVE_INFINITY;
+			if(ImGui.dragFloat4("##" + field.getName(), ptr, step, min, max)) {
+				field.set(component, new Vec4f(ptr[0], ptr[1], ptr[2], ptr[3]));
+			}
+		} else if(ImGui.inputFloat4("##" + field.getName(), ptr)) {
 			field.set(component, new Vec4f(ptr[0], ptr[1], ptr[2], ptr[3]));
 		}
 	}
 
 	private static void renderResource(Component component, Field field) throws IllegalAccessException {
 		Resource resource = (Resource) field.get(component);
-		ImString ptr = new ImString(Resources.pathOf(resource), 256);
-		if(ImGui.inputText("##" + field.getName(), ptr)) {
-			setResource(component, field, ptr.get());
+		String str = Resources.pathOf(resource);
+		if(field.isAnnotationPresent(DefaultValueString.class)) {
+			DefaultValueString defaultValue = field.getAnnotation(DefaultValueString.class);
+			if(str.equals(defaultValue.value()) && defaultValue.hide()) {
+				ImString ptr = new ImString("", 256);
+				if(ImGui.inputText("##" + field.getName(), ptr)) {
+					setResource(component, field, ptr.get());
+				}
+			} else {
+				ImString ptr = new ImString(str, 256);
+				if(ImGui.inputText("##" + field.getName(), ptr)) {
+					String val = ptr.get();;
+					setResource(component, field, val.isEmpty() ? defaultValue.value() : val);
+				}
+			}
+			// TODO: Add button to reset to default value
+		} else {
+			ImString ptr = new ImString(str, 256);
+			if(ImGui.inputText("##" + field.getName(), ptr)) {
+				setResource(component, field, ptr.get());
+			}
 		}
 		if(ImGui.beginDragDropTarget()) {
 			Object payload = ImGui.acceptDragDropPayload("Path");
@@ -171,15 +234,21 @@ public class InspectorGui implements IEditorGui {
 	}
 
 	private static void setResource(Component component, Field field, String value) throws IllegalAccessException {
-		if(value.contains(".") && Files.exists(Path.of("src/main/resources" + value))) {
-			try {
-				Resource newResource = Resources.getOrLoad(value);
-				if(newResource.getClass().isAssignableFrom(field.getType()))
+		try {
+			Resource newResource = Resources.getOrLoad(value);
+			if(newResource.getClass().isAssignableFrom(field.getType())) {
+				EditorVariable annotation = field.getAnnotation(EditorVariable.class);
+				if(!annotation.setter().isEmpty()) {
+					try {
+						component.getClass().getDeclaredMethod(annotation.setter(), newResource.getClass()).invoke(component, newResource);
+					} catch (InvocationTargetException | NoSuchMethodException e) {
+						e.printStackTrace();
+					}
+				} else {
 					field.set(component, newResource);
-			} catch(NoSuchElementException e) {
-				e.printStackTrace();
+				}
 			}
-		}
+		} catch(NoSuchElementException | UncheckedIOException ignored) {}
 	}
 
 	public void setEntity(Entity entity) {
---
+++ run/src/main/resources/shaders/test_shader.glsl
@@ -0,0 +1,14 @@
+#define VERTEX
+
+void vertex_shader() {
+
+}
+
+#undef VERTEX
+#define FRAGMENT
+
+vec4 fragment_shader() {
+    return vec4(1.0, 0.0, 0.0, 0.0);
+}
+
+#undef FRAGMENT
