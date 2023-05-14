a9e2720ffa57521a3103ad4e10c1e73db6197804
Nico <nico.hex6@gmail.com>
2023-05-14 20:21:44 +0200

    Removed unused gui set

--- editor/src/main/java/gamma/editor/gui/WindowGui.java
+++ editor/src/main/java/gamma/editor/gui/WindowGui.java
@@ -2,28 +2,7 @@ package gamma.editor.gui;
 
 import imgui.ImGui;
 
-import java.util.HashSet;
-
 public abstract class WindowGui extends EditorGui {
-
-	private static final HashSet<WindowGui> GUI = new HashSet<>();
-
-	public static void add(WindowGui gui) {
-		GUI.add(gui);
-	}
-
-	public static void clear() {
-		GUI.clear();
-	}
-
-	public static void drawGui() {
-		GUI.forEach(gui -> {
-			if(ImGui.begin(gui.title(), gui.flags())) {
-				gui.onDraw();
-			}
-			ImGui.end();
-		});
-	}
 
 	@Override
 	protected final void onDraw() {
